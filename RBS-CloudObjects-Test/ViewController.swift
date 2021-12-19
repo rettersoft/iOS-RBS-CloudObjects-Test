c631de5645e30047d7c5fa1cd89f63dfa26f9ab9
Baran Baygan <baran@rettermobile.com>
2021-12-19 17:18:41 +0300

    Initial Commit

---
+++ RBS-CloudObjects-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RBS-CloudObjects-Test
+//
+//  Created by Baran Baygan on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
